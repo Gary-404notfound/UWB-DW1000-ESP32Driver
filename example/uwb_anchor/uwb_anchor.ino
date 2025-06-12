0f8dd166bbbb77b57d9d71963c415f0e3fe04646
Gary404 <1420812669@qq.com>
2025-06-12 13:53:49 +0800

    add example

--- example/uwb_anchor/uwb_anchor.ino
+++ example/uwb_anchor/uwb_anchor.ino
@@ -1,67 +1,97 @@
-/*
+//anchor #4 setup
 
-For ESP32 UWB Pro with Display
 
-*/
+// be sure to edit anchor_addr and select the previously calibrated anchor delay
+// my naming convention is anchors 1, 2, 3, ... have the lowest order byte of the MAC address set to 81, 82, 83, ...
 
 #include <SPI.h>
 #include "DW1000Ranging.h"
+#include "DW1000.h"
 
-#include <Wire.h>
+// leftmost two bytes below will become the "short address"
+char anchor_addr[] = "84:00:5B:D5:A9:9A:E2:9C"; //#4
 
-#define ANCHOR_ADD "86:17:5B:D5:A9:9A:E2:9C"
+//calibrated Antenna Delay setting for this anchor
+uint16_t Adelay = 16580;
 
- // connection pins
-const uint8_t SPI_SCK  = 6;
-const uint8_t SPI_MISO = 7;
-const uint8_t SPI_MOSI = 8;
-const uint8_t PIN_RST  = 2;
-const uint8_t PIN_IRQ  = 5;
-const uint8_t PIN_SS   = 9;
+// previously determined calibration results for antenna delay
+// #1 16630
+// #2 16610
+// #3 16607
+// #4 16580
 
-void setup()
-{
-    Serial.begin(115200);
+// calibration distance
+float dist_m = (285 - 1.75) * 0.0254; //meters
 
-    delay(1000);
+#define SPI_SCK 6
+#define SPI_MISO 7
+#define SPI_MOSI 8
+#define DW_CS 9
 
-    // init the configuration
-    SPI.begin(SPI_SCK, SPI_MISO, SPI_MOSI);
-    DW1000Ranging.initCommunication(PIN_RST, PIN_SS, PIN_IRQ); // Reset, CS, IRQ pin
-
-    DW1000Ranging.attachNewRange(newRange);
-    DW1000Ranging.attachBlinkDevice(newBlink);
-    DW1000Ranging.attachInactiveDevice(inactiveDevice);
-
-    DW1000Ranging.startAsAnchor(ANCHOR_ADD, DW1000.MODE_LONGDATA_FAST_ACCURACY, false);
-}
-
-void loop()
-{
-    DW1000Ranging.loop();
-}
+// connection pins
+const uint8_t PIN_RST = 2; // reset pin
+const uint8_t PIN_IRQ = 5; // irq pin
+const uint8_t PIN_SS = 9;   // spi select pin
 
 void newRange()
 {
-    Serial.print("from: ");
-    Serial.print(DW1000Ranging.getDistantDevice()->getShortAddress(), HEX);
-    Serial.print("\t Range: ");
-    Serial.print(DW1000Ranging.getDistantDevice()->getRange());
-    Serial.print(" m");
-    Serial.print("\t RX power: ");
-    Serial.print(DW1000Ranging.getDistantDevice()->getRXPower());
-    Serial.println(" dBm");
+  Serial.print("from: ");
+  Serial.print(DW1000Ranging.getDistantDevice()->getShortAddress(), HEX);
+  Serial.print(", ");
+
+  #define NUMBER_OF_DISTANCES 1
+  float dist = 0.0;
+  for (int i = 0; i < NUMBER_OF_DISTANCES; i++) {
+    dist += DW1000Ranging.getDistantDevice()->getRange();
+  }
+  dist = dist/NUMBER_OF_DISTANCES;
+  Serial.print(dist);
+  Serial.println(" m");
 }
 
-void newBlink(DW1000Device *device)
+void newDevice(DW1000Device *device)
 {
-    Serial.print("blink; 1 device added ! -> ");
-    Serial.print(" short:");
-    Serial.println(device->getShortAddress(), HEX);
+  Serial.print("Device added: ");
+  Serial.println(device->getShortAddress(), HEX);
 }
 
 void inactiveDevice(DW1000Device *device)
 {
-    Serial.print("delete inactive device: ");
-    Serial.println(device->getShortAddress(), HEX);
+  Serial.print("Delete inactive device: ");
+  Serial.println(device->getShortAddress(), HEX);
+}
+
+void setup()
+{
+  Serial.begin(115200);
+  delay(1000); //wait for serial monitor to connect
+  Serial.println("Anchor config and start");
+  Serial.print("Antenna delay ");
+  Serial.println(Adelay);
+  Serial.print("Calibration distance ");
+  Serial.println(dist_m);
+
+  //init the configuration
+  SPI.begin(SPI_SCK, SPI_MISO, SPI_MOSI);
+  DW1000Ranging.initCommunication(PIN_RST, PIN_SS, PIN_IRQ); //Reset, CS, IRQ pin
+
+  // set antenna delay for anchors only. Tag is default (16384)
+  DW1000.setAntennaDelay(Adelay);
+
+  DW1000Ranging.attachNewRange(newRange);
+  DW1000Ranging.attachNewDevice(newDevice);
+  DW1000Ranging.attachInactiveDevice(inactiveDevice);
+
+  //start the module as an anchor, do not assign random short address
+  DW1000Ranging.startAsAnchor(anchor_addr, DW1000.MODE_LONGDATA_RANGE_LOWPOWER, false);
+  // DW1000Ranging.startAsAnchor(ANCHOR_ADD, DW1000.MODE_SHORTDATA_FAST_LOWPOWER);
+  // DW1000Ranging.startAsAnchor(ANCHOR_ADD, DW1000.MODE_LONGDATA_FAST_LOWPOWER);
+  // DW1000Ranging.startAsAnchor(ANCHOR_ADD, DW1000.MODE_SHORTDATA_FAST_ACCURACY);
+  // DW1000Ranging.startAsAnchor(ANCHOR_ADD, DW1000.MODE_LONGDATA_FAST_ACCURACY);
+  // DW1000Ranging.startAsAnchor(ANCHOR_ADD, DW1000.MODE_LONGDATA_RANGE_ACCURACY);
+}
+
+void loop()
+{
+  DW1000Ranging.loop();
 }
--- example/uwb_tag/uwb_tag.ino
+++ example/uwb_tag/uwb_tag.ino
@@ -1,238 +1,65 @@
-#include <Arduino.h>
+// currently tag is module #5
+// The purpose of this code is to set the tag address and antenna delay to default.
+// this tag will be used for calibrating the anchors.
 
 #include <SPI.h>
 #include "DW1000Ranging.h"
+#include "DW1000.h"
 
-#define TAG_ADDR "7D:00:22:EA:82:60:3B:9B"
+#define SPI_SCK 6
+#define SPI_MISO 7
+#define SPI_MOSI 8
+#define DW_CS 9
 
- // connection pins
-const uint8_t SPI_SCK  = 6;
-const uint8_t SPI_MISO = 7;
-const uint8_t SPI_MOSI = 8;
-const uint8_t PIN_RST  = 2;
-const uint8_t PIN_IRQ  = 5;
-const uint8_t PIN_SS   = 9;
-long int runtime = 0;
+// connection pins
+const uint8_t PIN_RST = 2; // reset pin
+const uint8_t PIN_IRQ = 5; // irq pin
+const uint8_t PIN_SS = 9;   // spi select pin
 
-struct Link
-{
-    uint16_t anchor_addr;
-    float range;
-    float dbm;
-    struct Link *next;
-};
-
-struct Link *uwb_data;
-
-
-struct Link *init_link()
-{
-#ifdef DEBUG
-    Serial.println("init_link");
-#endif
-    struct Link *p = (struct Link *)malloc(sizeof(struct Link));
-    p->next = NULL;
-    p->anchor_addr = 0;
-    p->range = 0.0;
-
-    return p;
-}
-
-void add_link(struct Link *p, uint16_t addr)
-{
-#ifdef DEBUG
-    Serial.println("add_link");
-#endif
-    struct Link *temp = p;
-    // Find struct Link end
-    while (temp->next != NULL)
-    {
-        temp = temp->next;
-    }
-
-    Serial.println("add_link:find struct Link end");
-    // Create a anchor
-    struct Link *a = (struct Link *)malloc(sizeof(struct Link));
-    a->anchor_addr = addr;
-    a->range = 0.0;
-    a->dbm = 0.0;
-    a->next = NULL;
-
-    // Add anchor to end of struct Link
-    temp->next = a;
-
-    return;
-}
-
-struct Link *find_link(struct Link *p, uint16_t addr)
-{
-#ifdef DEBUG
-    Serial.println("find_link");
-#endif
-    if (addr == 0)
-    {
-        Serial.println("find_link:Input addr is 0");
-        return NULL;
-    }
-
-    if (p->next == NULL)
-    {
-        Serial.println("find_link:Link is empty");
-        return NULL;
-    }
-
-    struct Link *temp = p;
-    // Find target struct Link or struct Link end
-    while (temp->next != NULL)
-    {
-        temp = temp->next;
-        if (temp->anchor_addr == addr)
-        {
-            // Serial.println("find_link:Find addr");
-            return temp;
-        }
-    }
-
-    Serial.println("find_link:Can't find addr");
-    return NULL;
-}
-
-void fresh_link(struct Link *p, uint16_t addr, float range, float dbm)
-{
-#ifdef DEBUG
-    Serial.println("fresh_link");
-#endif
-    struct Link *temp = find_link(p, addr);
-    if (temp != NULL)
-    {
-
-        temp->range = range;
-        temp->dbm = dbm;
-        return;
-    }
-    else
-    {
-        Serial.println("fresh_link:Fresh fail");
-        return;
-    }
-}
-
-void print_link(struct Link *p)
-{
-#ifdef DEBUG
-    Serial.println("print_link");
-#endif
-    struct Link *temp = p;
-
-    while (temp->next != NULL)
-    {
-        // Serial.println("Dev %d:%d m", temp->next->anchor_addr, temp->next->range);
-        Serial.println(temp->next->anchor_addr, HEX);
-        Serial.println(temp->next->range);
-        Serial.println(temp->next->dbm);
-        temp = temp->next;
-    }
-
-    return;
-}
-
-void delete_link(struct Link *p, uint16_t addr)
-{
-#ifdef DEBUG
-    Serial.println("delete_link");
-#endif
-    if (addr == 0)
-        return;
-
-    struct Link *temp = p;
-    while (temp->next != NULL)
-    {
-        if (temp->next->anchor_addr == addr)
-        {
-            struct Link *del = temp->next;
-            temp->next = del->next;
-            free(del);
-            return;
-        }
-        temp = temp->next;
-    }
-    return;
-}
+// TAG antenna delay defaults to 16384
+// leftmost two bytes below will become the "short address"
+char tag_addr[] = "7D:00:22:EA:82:60:3B:9C";
 
 void newRange()
 {
-    Serial.print("from: ");
-    Serial.print(DW1000Ranging.getDistantDevice()->getShortAddress(), HEX);
-    Serial.print("\t Range: ");
-    Serial.print(DW1000Ranging.getDistantDevice()->getRange());
-    Serial.print(" m");
-    Serial.print("\t RX power: ");
-    Serial.print(DW1000Ranging.getDistantDevice()->getRXPower());
-    Serial.println(" dBm");
-
-    fresh_link(uwb_data, DW1000Ranging.getDistantDevice()->getShortAddress(), DW1000Ranging.getDistantDevice()->getRange(), DW1000Ranging.getDistantDevice()->getRXPower());
+  Serial.print("from: ");
+  Serial.print(DW1000Ranging.getDistantDevice()->getShortAddress(), HEX);
+  Serial.print(", ");
+  Serial.print(DW1000Ranging.getDistantDevice()->getRange());
+  Serial.println(" m");
 }
 
 void newDevice(DW1000Device *device)
 {
-    Serial.print("ranging init; 1 device added ! -> ");
-    Serial.print(" short:");
-    Serial.println(device->getShortAddress(), HEX);
-
-    add_link(uwb_data, device->getShortAddress());
+  Serial.print("Device added: ");
+  Serial.println(device->getShortAddress(), HEX);
 }
 
 void inactiveDevice(DW1000Device *device)
 {
-    Serial.print("delete inactive device: ");
-    Serial.println(device->getShortAddress(), HEX);
-
-    delete_link(uwb_data, device->getShortAddress());
+  Serial.print("delete inactive device: ");
+  Serial.println(device->getShortAddress(), HEX);
 }
 
 void setup()
 {
-    Serial.begin(115200);
-    delay(5000);
-    Serial.println("begin config");
-    //init the configuration
-    SPI.begin(SPI_SCK, SPI_MISO, SPI_MOSI);
-    // SPI.beginTransaction(SPISettings(2000000, MSBFIRST, SPI_MODE0));
-    DW1000Ranging.initCommunication(PIN_RST, PIN_SS, PIN_IRQ); //Reset, CS, IRQ pin
-    //define the sketch as anchor. It will be great to dynamically change the type of module
-    DW1000Ranging.attachNewRange(newRange);
-    DW1000Ranging.attachNewDevice(newDevice);
-    DW1000Ranging.attachInactiveDevice(inactiveDevice);
-    //Enable the filter to smooth the distance
-    //DW1000Ranging.useRangeFilter(true);
+  Serial.begin(115200);
+  delay(2000);
 
-    //we start the module as a tag
-    DW1000Ranging.startAsTag(TAG_ADDR, DW1000.MODE_LONGDATA_FAST_ACCURACY, false);
-    Serial.println("end config");
-    uwb_data = init_link();
+  //init the configuration
+  SPI.begin(SPI_SCK, SPI_MISO, SPI_MOSI);
+  DW1000Ranging.initCommunication(PIN_RST, PIN_SS, PIN_IRQ); //Reset, CS, IRQ pin
+
+  DW1000Ranging.attachNewRange(newRange);
+  DW1000Ranging.attachNewDevice(newDevice);
+  DW1000Ranging.attachInactiveDevice(inactiveDevice);
+
+// start as tag, do not assign random short address
+
+  DW1000Ranging.startAsTag(tag_addr, DW1000.MODE_LONGDATA_RANGE_LOWPOWER, false);
 }
 
 void loop()
 {
-    DW1000Ranging.loop();
-    // if ((millis() - runtime) > 1000)
-    // {
-    //     display_uwb(uwb_data);
-    //     runtime = millis();
-    // }
-}
-
-void display_uwb(struct Link *p)
-{
-    struct Link *temp = p;
-    if (temp->next == NULL){
-        return;
-    };
-    while (temp->next != NULL){
-        temp = temp->next;
-
-        // Serial.println("Dev %d:%d m", temp->next->anchor_addr, temp->next->range);
-        Serial.println(temp->anchor_addr, HEX);
-        Serial.println(temp->range);
-    }
+  DW1000Ranging.loop();
 }
